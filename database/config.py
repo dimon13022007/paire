2e60d7b0071753b1f91961ec33174be4a59aeaee
dimon13022007 <dimon13022007@gmail.com>
2025-04-24 12:21:31 +0300

    🔒 Добавлен CA сертификат для подключения к БД

--- database/config.py
+++ database/config.py
@@ -14,7 +14,10 @@ class Settings(BaseSettings):
 
     @property
     def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- database/engine.py
+++ database/engine.py
@@ -1,3 +1,4 @@
+import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from .config import settings
 from sqlalchemy.ext.asyncio import async_sessionmaker
@@ -5,9 +6,16 @@ from sqlalchemy.orm import DeclarativeBase
 from typing import Annotated
 from sqlalchemy import String
 
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
-    echo=False
+    echo=False,
+    connect_args={"ssl": ssl_context}
 )
 
 async_sessions = async_sessionmaker(async_engine, expire_on_commit=False)
@@ -22,4 +30,3 @@ class Base(DeclarativeBase):
     type_anotation_map = {
         str_256: String(256)
     }
-
